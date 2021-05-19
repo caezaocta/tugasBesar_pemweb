e4a1c1075833ee442951925df460f987f8f9318d
snaztoz <hafidhmn@gmail.com>
2021-05-19 20:10:19 +0700

    Membuat beberapa tabel baru.

---
+++ database/migrations/2021_05_05_155911_create_skp_realisasi_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSkpRealisasiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skp_realisasi', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal_awal');
+            $table->date('tanggal_akhir');
+            $table->string('lokasi');
+            $table->integer('jml_realisasi');
+            $table->string('keterangan', 100);
+            $table->string('path_bukti');
+
+            $table->timestamps();
+            $table
+                ->foreignId('id_skp_target')
+                ->constrained('skp_target')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table
+                ->foreignId('created_by')
+                ->constrained('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table
+                ->foreignId('updated_by')
+                ->constrained('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skp_realisasi');
+    }
+}
