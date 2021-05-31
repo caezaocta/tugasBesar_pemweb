54085639d5551f62154434daf139638d5655cbc5
snaztoz <hafidhmn@gmail.com>
2021-05-31 15:10:59 +0700

    Mengubah spacing navbar.

--- resources/views/components/units/navbar.blade.php
+++ resources/views/components/units/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+<nav class="navbar navbar-expand-lg navbar-light px-md-2 px-lg-3 bg-light justify-content-between">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Sistem SKP</a>
 
