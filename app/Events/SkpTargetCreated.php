a05dead6fccca16d8a8294930f084dfb481f63bb
snaztoz <hafidhmn@gmail.com>
2021-06-01 22:27:18 +0700

    Menambahkan data di event.

--- app/Events/SkpTargetCreated.php
+++ app/Events/SkpTargetCreated.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\SkpTarget;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -14,14 +15,16 @@ class SkpTargetCreated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public $skp_target;
+
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(SkpTarget $skp_target)
     {
-        //
+        $this->skp_target = $skp_target;
     }
 
     /**
