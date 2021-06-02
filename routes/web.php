b4fef0f33ab38643daadd1e8ff5bb13e049bd3d2
lutfiana07 <lutfianarfh01@gmail.com>
2021-06-03 02:44:39 +0700

    route ref units

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/refunits', 'App\Http\Controllers\RefUnitsController@index');
-
-Route::get('/refunits/create', 'App\Http\Controllers\RefUnitsController@create');
-
-Route::get('/refunits/{refUnit}', 'App\Http\Controllers\RefUnitsController@show');
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 Route::resource('uraian-pekerjaan', UraianPekerjaanController::class)
         ->except(['show']);
+
+Route::resource('ref-unit', RefUnitsController::class);        
 
 require __DIR__.'/auth.php';
