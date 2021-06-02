27a632b1c65c88c545eab47bad851aa6cb87106e
lutfiana07 <lutfianarfh01@gmail.com>
2021-06-03 02:52:53 +0700

    routes ref-unit yang ketiga

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\UraianPekerjaanController;
-use App\Http\Controllers\RefUnitController;
+use App\Http\Controllers\RefUnitsController;
 use Illuminate\Support\Facades\Route;
 
 /*
