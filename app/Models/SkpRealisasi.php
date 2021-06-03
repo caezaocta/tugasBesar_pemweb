ea5c52d4f4671bc2004d47025d24a8852a527dae
snaztoz <hafidhmn@gmail.com>
2021-06-03 12:29:02 +0700

    Memasukkan kolom fillable.

--- app/Models/SkpRealisasi.php
+++ app/Models/SkpRealisasi.php
@@ -11,6 +11,18 @@ class SkpRealisasi extends Model
 
     protected $table = 'skp_realisasi';
 
+    protected $fillable = [
+        'id_skp_target',
+        'tanggal_awal',
+        'tanggal_akhir',
+        'lokasi',
+        'jml_realisasi',
+        'keterangan',
+        'path_bukti',
+        'created_by',
+        'updated_by',
+    ];
+
     public function skp_target() {
         return $this->belongsTo(SkpTarget::class, 'id_skp_target');
     }
