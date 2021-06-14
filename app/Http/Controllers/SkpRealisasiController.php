f1b3de0086cdaab71e07f1d65e4b0c78a7bcb386
snaztoz <hafidhmn@gmail.com>
2021-06-14 16:08:20 +0700

    Membuat page edit dan handler update.

--- app/Http/Controllers/SkpRealisasiController.php
+++ app/Http/Controllers/SkpRealisasiController.php
@@ -6,6 +6,7 @@ use App\Models\SkpRealisasi;
 use App\Models\SkpTarget;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class SkpRealisasiController extends Controller
 {
@@ -96,7 +97,35 @@ class SkpRealisasiController extends Controller
      */
     public function update(Request $request, SkpRealisasi $skpRealisasi)
     {
-        //
+        $validated = $request->validate([
+            'lokasi' => 'nullable|max:255',
+            'jml_realisasi' => [
+                'required',
+                // Nilai jml_realisasi harus berada di rentang 0 <= x <= jml_target
+                function ($attr, $val, $fail) use ($skpRealisasi) {
+                    $jml_target = $skpRealisasi->skp_target()->first()->jml_target;
+                    if ($val < 0 || $val > $jml_target) {
+                        $fail($attr.' must in between 0 and its target\'s amount');
+                    }
+                }
+            ],
+            'keterangan' => 'nullable|max:100',
+            'bukti' => 'nullable|mimes:jpg,png,doc,docx,pdf,zip',
+        ]);
+
+        // upload file bukti
+        $path_bukti = Storage::putFile('public/bukti', $validated['bukti']);
+        $path_bukti = Storage::url(str_replace('public', '', $path_bukti));
+
+        $skpRealisasi->fill([
+            'lokasi' => $validated['lokasi'],
+            'jml_realisasi' => $validated['jml_realisasi'],
+            'keterangan' => $validated['keterangan'],
+            'path_bukti' => $path_bukti,
+            'updated_by' => $request->user()->id
+        ])->save();
+
+        return redirect()->route('skp-realisasi.index');
     }
 
     /**
