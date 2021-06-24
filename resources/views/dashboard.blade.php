b6e7e1112c041ae17e845dbde8757ffd289a09c8
snaztoz <hafidhmn@gmail.com>
2021-06-24 10:16:12 +0700

    Menambahkan menu di dashboard.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,36 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <h1 class="text-2xl">Selamat datang, {{ Auth::user()->name }}!</h1>
+
+                    <div class="mt-3 p-5">
+                        @php
+                            $menus = [
+                                ['nama' => 'Manajemen Target SKP', 'url' => '/skptargets'],
+                                ['nama' => 'Manajemen Realisasi SKP', 'url' => '/skp-realisasi'],
+                            ];
+
+                            if (Auth::user()->is_admin()) {
+                                $menus = array_merge($menus, [
+                                    ['nama' => 'Manajemen Pegawai', 'url' => '/pegawai'],
+                                    ['nama' => 'Manajemen Unit', 'url' => '/refunits'],
+                                    ['nama' => 'Manajemen Jabatan', 'url' => '#'],
+                                    ['nama' => 'Manajemen Uraian Pekerjaan', 'url' => '/uraian-pekerjaan'],
+                                    ['nama' => 'Manajemen Uraian Pekerjaan per Jabatan', 'url' => '/uraian-pekerjaan-jabatan'],
+                                    ['nama' => 'Laporan Poin tiap Unit', 'url' => route('perolehan-poin-tiap-unit')],
+                                ]);
+                            }
+                        @endphp
+
+                        <p>Kamu ingin kemana?</p>
+                        <ul class="mt-2">
+                        @foreach ($menus as $menu)
+                            <li class="text-blue-500 hover:text-blue-700 text-underline">
+                                <a href="{{ $menu['url'] }}">{{ $menu['nama'] }}</a>
+                            </li>
+                        @endforeach
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
