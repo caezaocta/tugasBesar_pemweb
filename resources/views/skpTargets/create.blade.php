8fc58a4a7a7511d0fb4e6a1d55558583b328cbef
snaztoz <hafidhmn@gmail.com>
2021-06-24 14:43:10 +0700

    Mengubah sedikit style.

--- resources/views/skpTargets/create.blade.php
+++ resources/views/skpTargets/create.blade.php
@@ -13,30 +13,26 @@
                     @csrf
 
                     <div class="mb-3">
-                        <label for="id_periode">ID Periode</label>
-                        <select class="form-select" aria-label="Default select example" id="id_periode"
-                            name="id_periode">
+                        <label class="form-label" for="id_periode">ID Periode</label>
+                        <select class="form-select" aria-label="Default select example"
+                                id="id_periode" name="id_periode">
                             @foreach ($daftarPeriode as $namaPeriode)
                                 <option value="{{ $namaPeriode->id }}">{{ $namaPeriode->nama }}</option>
                             @endforeach
-
                         </select>
                     </div>
 
                     <div class="mb-3">
-
-                        <label for="id_uraian_pekerjaan_jabatan">Uraian Pekerjaan</label>
-                        <select class="form-select" aria-label="Default select example" id="id_uraian_pekerjaan_jabatan"
-                            name="id_uraian_pekerjaan_jabatan">
+                        <label class="form-label" for="id_uraian_pekerjaan_jabatan">Uraian Pekerjaan</label>
+                        <select class="form-select" aria-label="Default select example"
+                                id="id_uraian_pekerjaan_jabatan" name="id_uraian_pekerjaan_jabatan">
                             @foreach ($daftarUraianPekerjaan as $namaPekerjaan)
                                 <option value="{{ $namaPekerjaan->id }}">{{ $namaPekerjaan->uraian }}</option>
                             @endforeach
-
                         </select>
                     </div>
 
-                    {{-- <input type="submit" class="btn btn-primary rounded-pill" value="Tambah"> --}}
-                    <button type="submit" class="btn btn-primary rounded-pill">Tambah</button>
+                    <button type="submit" class="mt-3 btn btn-primary rounded-pill">Tambah</button>
 
                 </form>
 
